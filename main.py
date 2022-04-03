8fe52bbca8f545366314ea61d3aeaba416b8f4f2
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-04-03 23:53:07 +0800

    Hope everything ok

--- main.py
+++ main.py
@@ -62,6 +62,40 @@ load_dotenv()
 # list_zh_tw = load_command.read_description("list", "zh-tw")
 # random_zh_tw = load_command.read_description("random", "zh-tw")
 
+def process_voice(content: str, lang_code: str):
+    """Synthesizes speech from the input string of text or ssml.
+    Make sure to be working in a virtual environment.
+
+    Note: ssml must be well-formed according to:
+        https://www.w3.org/TR/speech-synthesis/
+    """
+    import os
+    from google.cloud import texttospeech
+
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=content)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code=lang_code, ssml_gender=texttospeech.SsmlVoiceGender.SSML_VOICE_GENDER_UNSPECIFIED
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(
+        input=synthesis_input, voice=voice, audio_config=audio_config
+    )
+    return response.audio_content
+
 
 @bot.event
 # 當機器人完成啟動時
@@ -165,11 +199,8 @@ async def say(ctx, *, content: str):  # sourcery skip: for-index-replacement
                 # export content to mp3 by google tts api
                 print("init google tts api")
                 # tts_func.process_voice(content, db["lang"])
-                subprocess.call(["python", "tts_alone.py", "--content", content, "--lang", db["lang"]])
-
-                # play mp3
                 print("play mp3")
-                voice_file = discord.FFmpegPCMAudio(f"{os.environ['TEMP']}/output.mp3")
+                voice_file = discord.FFmpegPCMAudio(process_voice(content, db["lang"]))
                 if not ctx.voice_client.is_playing():
                     ctx.voice_client.play(voice_file, after=None)
             else:
