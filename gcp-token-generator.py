0d8e023152054a1b8ff23c1d847bbdd4e80470e7
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-05-15 13:30:13 +0800

    Working for migrate to heroku

---
+++ gcp-token-generator.py
@@ -0,0 +1,13 @@
+import json
+import os
+
+import dotenv
+
+dotenv.load_dotenv()
+
+GCP_TOKEN = os.getenv("GCP_TOKEN")
+GCP_JSON = json.loads(GCP_TOKEN)
+
+# dump the json to a file
+with open('gcp-token.json', 'w') as f:
+    json.dump(GCP_JSON, f, indent=4)
