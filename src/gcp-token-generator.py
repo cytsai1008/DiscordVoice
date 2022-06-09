df6b011a08cb028d41857408678d678ab8aaaa8d
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-06-09 10:45:27 +0800

    :question:

--- src/gcp-token-generator.py
+++ src/gcp-token-generator.py
@@ -5,9 +5,9 @@ import dotenv
 
 dotenv.load_dotenv()
 
-GCP_TOKEN = os.getenv("GCP_TOKEN")
+GCP_TOKEN = os.environ["GCP_TOKEN"]
 GCP_JSON = json.loads(GCP_TOKEN)
 
 # dump the json to a file
 with open("gcp-token.json", "w") as f:
-    json.dump(GCP_JSON, f, indent=4)
+    json.dump(GCP_JSON, f, indent=2)
