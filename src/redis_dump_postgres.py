aee38a83bac44c6391942fec119728c1c703cc8c
CYTsai <47322522+cytsai1008@users.noreply.github.com>
2022-06-11 03:28:50 +0800

    Fix wrong table

--- src/redis_dump_postgres.py
+++ src/redis_dump_postgres.py
@@ -8,20 +8,20 @@ import redis
 dotenv.load_dotenv()
 
 dv_redis = redis.Redis(
-        host=os.environ["REDIS_DV_URL"],
-        port=16704,
-        username=os.environ["REDIS_USER"],
-        password=os.environ["REDIS_DV_PASSWD"],
-        decode_responses=True,
-    )
+    host=os.environ["REDIS_DV_URL"],
+    port=16704,
+    username=os.environ["REDIS_USER"],
+    password=os.environ["REDIS_DV_PASSWD"],
+    decode_responses=True,
+)
 
 wfnm_redis = redis.Redis(
-        host=os.environ["REDIS_WFNM_URL"],
-        port=16062,
-        username=os.environ["REDIS_USER"],
-        password=os.environ["REDIS_WFNM_PASSWD"],
-        decode_responses=True,
-    )
+    host=os.environ["REDIS_WFNM_URL"],
+    port=16062,
+    username=os.environ["REDIS_USER"],
+    password=os.environ["REDIS_WFNM_PASSWD"],
+    decode_responses=True,
+)
 
 heroku_postgres = psycopg2.connect(os.environ["DATABASE_URL"], sslmode="require")
 cur = heroku_postgres.cursor()
@@ -32,39 +32,37 @@ else:
     print("Running in production environment")
     print("This should be a scheduled job")
 
-
 dv_dump_data = {}
 print("Dumping DV data")
-keys = dv_redis.keys('*')
+keys = dv_redis.keys("*")
 for key in keys:
     key_type = dv_redis.type(key)
-    if key_type == 'ReJSON-RL':
+    if key_type == "ReJSON-RL":
         data: dict = dv_redis.json().get(key)
 
         # append data to dump_data
         dv_dump_data[key] = data
 
-with open('db_dump/dv_dump_data.json', 'w') as f:
+with open("db_dump/dv_dump_data.json", "w") as f:
     json.dump(dv_dump_data, f, indent=2)
-
 
 wfnm_dump_data = {}
 print("Dumping WFNM data")
-keys = wfnm_redis.keys('*')
+keys = wfnm_redis.keys("*")
 for key in keys:
     key_type = wfnm_redis.type(key)
-    if key_type == 'ReJSON-RL':
+    if key_type == "ReJSON-RL":
         data: dict = wfnm_redis.json().get(key)
 
         # append data to dump_data
         wfnm_dump_data[key] = data
 
-with open('db_dump/wfnm_dump_data.json', 'w') as f:
+with open("db_dump/wfnm_dump_data.json", "w") as f:
     json.dump(wfnm_dump_data, f, indent=2)
 
 # create table if not exists
-cur.execute("CREATE TABLE IF NOT EXISTS dv_dump_data (key TEXT, data TEXT);")
-cur.execute("CREATE TABLE IF NOT EXISTS wfnm_dump_data (key TEXT, data TEXT);")
+cur.execute("CREATE TABLE IF NOT EXISTS dv_dump_data (data TEXT);")
+cur.execute("CREATE TABLE IF NOT EXISTS wfnm_dump_data (data TEXT);")
 heroku_postgres.commit()
 
 print("Dumping DV data to Postgres")
@@ -76,3 +74,5 @@ print("Dumping WFNM data to Postgres")
 postgres_wfnm_data = json.dumps(wfnm_dump_data)
 cur.execute("INSERT INTO wfnm_dump_data (data) VALUES (%s)", (postgres_wfnm_data,))
 heroku_postgres.commit()
+
+heroku_postgres.close()
